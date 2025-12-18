fd22d253b8ffc6fc2968ac9f2cd29253dc275299
horransun <horransun@tencent.com>
2025-12-18 15:58:54 +0800

    add 推理sh脚本

---
+++ prune_test.sh
@@ -0,0 +1,7 @@
+python3 clip_benchmark/cli.py eval \
+  --pretrained "openai/ViT-B-32" \  
+  --language "en" \
+  --task "zeroshot_classification"  \
+  --dataset "cifar10"  \
+  --dataset_root "https://huggingface.co/datasets/clip-benchmark/wds_{dataset_cleaned}/tree/main"
+
