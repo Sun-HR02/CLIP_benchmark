86031d36cebb29179631ac5650d2d002165922d6
horransun <horransun@tencent.com>
2025-12-18 16:57:52 +0800

    改指令

--- prune_test.sh
+++ prune_test.sh
@@ -6,8 +6,6 @@ python3 clip_benchmark/cli.py eval \
   --task "zeroshot_classification"  \
   --dataset "cifar10"  \
   --dataset_root "https://huggingface.co/datasets/clip-benchmark/wds_{dataset_cleaned}/tree/main" \
-  # --extract_attention \
-  # --attention_output "my_attention_weights.pt" \
   --enable_pruning \
   --k_anchors 10 \
   --top_m 50 \
